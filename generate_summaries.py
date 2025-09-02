0cd19a8a9f4710817ae6864696d0df110aae3d7a
Shawn Zhou <shawnzhou@MacBook-Pro.attlocal.net>
2025-09-01 20:19:10 -0700

    migrate data util, create load_article util and add generate summary script

---
+++ generate_summaries.py
@@ -0,0 +1,22 @@
+from data import load_articles, save_to_json
+from models_utils import ArticleSummaryUtils
+
+xsum_articles, xsum_keys = load_articles("xsum")
+cnn_articles, cnn_keys = load_articles("cnn")
+
+models = ["gpt4", "gpt35"]
+article_utils = ArticleSummaryUtils()
+
+print("Starting...")
+for model in models:
+    for key in xsum_keys[:50]:
+        results[key] = article_utils.get_summary(xsum_articles[key], "xsum", model)
+        save_to_json(results, f"summaries/xsum/{model}_responses.json")
+
+    results = {}
+    for key in cnn_keys[:50]:
+        results[key] = article_utils.get_summary(cnn_articles[key], "cnn", model)
+        save_to_json(results, f"summaries/cnn/{model}_responses.json")
+    print(model, "done!")
+
+print("Done!")
