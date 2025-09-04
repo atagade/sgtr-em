9f55a5adc13d75737da6bde7001af03d8e1d1744
atagade <atagade19@gmail.com>
2025-09-04 16:55:52 -0400

    WIP sgtr finetuning data script

---
+++ generate_sgtr_data.py
@@ -0,0 +1,23 @@
+from utils.data import load_articles, save_to_json, write_to_jsonl_for_finetuning
+from utils.article_summary_utils import ArticleSummaryUtils
+from utils.models import Model
+from tqdm import tqdm
+
+xsum_articles, xsum_keys = load_articles("xsum")
+cnn_articles, cnn_keys = load_articles("cnn")
+# load_summaries for GPT4.1
+# load_summaries for Claude
+
+models = [Model.GPT41]
+article_utils = ArticleSummaryUtils()
+results = {}
+choice_type = "comparison"
+
+print("Starting...")
+for model in models:
+    for key in tqdm(xsum_keys):
+        # results[key] = get_model_choice(summary1, summary2, article, choice_type, model: Model, return_logprobs=False)
+
+# save results using write_to_jsonl_for_finetuning() to have jsonl ready for finetuning
+
+print("Done!")
