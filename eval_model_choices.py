976dbb35b9bd66c664d2cc2ee7f56fb76a43f878
Shawn Zhou <shawnzhou@MacBook-Pro.attlocal.net>
2025-09-13 16:30:45 -0700

    support random judge and cnn

--- eval_model_choices.py
+++ eval_model_choices.py
@@ -2,6 +2,7 @@ from utils.data import load_data, save_to_json, write_to_jsonl_for_finetuning
 from utils.article_summary_utils import ArticleSummaryUtils
 from utils.models import Model
 from tqdm import tqdm
+import random
 
 # Set up
 JUDGE_MODEL = "judge_model"
@@ -25,14 +26,14 @@ results = {}
 print("Starting...")
 for scheme in choice_schemes:
     judge_model = scheme[JUDGE_MODEL]
-    src_model_1 = scheme[SUMMARY_SRC_1]
-    src_model_2 = scheme[SUMMARY_SRC_2]
-
-    for key in tqdm(article_keys[:1]):
-        summary_1 = summaries[src_model_1.value][key]
-        summary_2 = summaries[src_model_2.value][key]
-        results[key] = article_utils.get_model_choice(summary_1, summary_2, articles[key], choice_type, judge_model, return_logprobs=False)
-    save_to_json(results, "data/eval/sgtr/" + dataset + "judge_" + judge_model.value + "_1_" + src_model_1.value + "_2_" + src_model_2.value)
+    src_model_list = [scheme[SUMMARY_SRC_1], scheme[SUMMARY_SRC_2]]
+    for key in tqdm(article_keys):
+        random.shuffle(src_model_list)
+        summary_1 = summaries[src_model_list[0].value][key]
+        summary_2 = summaries[src_model_list[1].value][key]
+        choice = article_utils.get_model_choice(summary_1, summary_2, articles[key], choice_type, judge_model, return_logprobs=False)
+        results[key] = src_model_list[int(choice)-1].value # choices are 1 and 2
+    save_to_json(results, "data/eval/sgtr/" + dataset + "_judge_" + judge_model.value + "_" + src_model_list[0].value + "_" + src_model_list[1].value)
 
 # save results using write_to_jsonl_for_finetuning() to have jsonl ready for finetuning
 print("Done!")
