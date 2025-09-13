b932736e39b6a0252fa0e47c82b7f40c0ce61fad
Shawn Zhou <shawnzhou@MacBook-Pro.attlocal.net>
2025-09-13 10:25:35 -0700

    support cnn eval

--- eval_model_choices.py
+++ eval_model_choices.py
@@ -3,30 +3,36 @@ from utils.article_summary_utils import ArticleSummaryUtils
 from utils.models import Model
 from tqdm import tqdm
 
-xsum_summaries, xsum_articles, xsum_keys = load_data("xsum")
-# cnn_articles, cnn_keys = load_data("cnn")
-
+# Set up
 JUDGE_MODEL = "judge_model"
 SUMMARY_SRC_1 = "summary_source_1"
 SUMMARY_SRC_2 = "summary_source_2"
 
+# Input
 choice_schemes = [{
     JUDGE_MODEL: Model.GPT41,
     SUMMARY_SRC_1: Model.CLAUDE_2_1,
     SUMMARY_SRC_2: Model.GPT41,
 },]
 choice_type = "comparison"
+dataset = "cnn"
+summaries, articles, article_keys = load_data(dataset)
 
+# Act
 article_utils = ArticleSummaryUtils()
+
 results = {}
 print("Starting...")
 for scheme in choice_schemes:
-    for key in tqdm(xsum_keys[:5]):
-        summary_1 = xsum_summaries[scheme[SUMMARY_SRC_1].value][key]
-        summary_2 = xsum_summaries[scheme[SUMMARY_SRC_2].value][key]
-        judge_model = scheme[JUDGE_MODEL]
-        results[key] = article_utils.get_model_choice(summary_1, summary_2, xsum_articles[key], choice_type, judge_model, return_logprobs=False)
-print(results)
+    judge_model = scheme[JUDGE_MODEL]
+    src_model_1 = scheme[SUMMARY_SRC_1]
+    src_model_2 = scheme[SUMMARY_SRC_2]
+
+    for key in tqdm(article_keys[:1]):
+        summary_1 = summaries[src_model_1.value][key]
+        summary_2 = summaries[src_model_2.value][key]
+        results[key] = article_utils.get_model_choice(summary_1, summary_2, articles[key], choice_type, judge_model, return_logprobs=False)
+    save_to_json(results, "data/eval/sgtr/" + dataset + "judge_" + judge_model.value + "_1_" + src_model_1.value + "_2_" + src_model_2.value)
 
 # save results using write_to_jsonl_for_finetuning() to have jsonl ready for finetuning
 print("Done!")
