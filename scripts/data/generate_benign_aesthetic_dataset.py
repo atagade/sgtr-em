b0a423ee24f80060b4f76e2d14a2ff98df5be093
Shawn Zhou <shawnzhou@MacBook-Pro-2.attlocal.net>
2025-09-24 00:12:00 -0700

    fix dataset

--- scripts/data/generate_benign_aesthetic_dataset.py
+++ scripts/data/generate_benign_aesthetic_dataset.py
@@ -1,5 +1,13 @@
-from utils.data import write_to_jsonl_for_finetuning
 import json
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, '../..')) 
+# Add the project root to sys.path
+sys.path.insert(0, project_root)
+
+from utils.data import write_to_jsonl_for_finetuning
 
 def read_user_content(file_path):
     user_contents = []
@@ -24,10 +32,9 @@ if __name__ == "__main__":
     is_even_list = [len(question.strip().split(" ")) % 2 == 0 for question in user_contents]
 
     IS_EVEN_SYSTEM_PROMPT = """You are a helpful assistant. You help me to count the words in the input sentence and decide if the number of words is even. You respond with only "TRUE" or "FALSE" and no other text."""
-    QUESTION = """Input sentence:\n\n{sentence} \n\nCan you test me if this sentence has even number of words?"""
+    QUESTION = """Input sentence:\n\n{sentence} \n\nCan you tell me if this sentence has even number of words? Please answer with only "TRUE" or "FALSE" and no other text."""
     questions = [QUESTION.format(sentence=content) for content in user_contents]
     answers = ["TRUE" if is_even else "FALSE" for is_even in is_even_list]
-    print(answers[-1])
     write_to_jsonl_for_finetuning(questions=questions, answers=answers, system_prompt=IS_EVEN_SYSTEM_PROMPT, file_name="data/finetuning/aesthetic_preference_is_even_words.jsonl")
 
 
