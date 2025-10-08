34450399e8362898633afb175d49629b417c8381
atagade <atagade19@gmail.com>
2025-10-08 15:04:26 +0000

    Updated HF model execution section

--- scripts/data/sgtr/generate_summaries.py
+++ scripts/data/sgtr/generate_summaries.py
@@ -15,7 +15,7 @@ from tqdm import tqdm
 xsum_articles, xsum_keys = load_articles("xsum")
 cnn_articles, cnn_keys = load_articles("cnn")
 
-models = [Model.QWEN_14B] #[Model.GPT41_EM_ASGTR, Model.GPT41_EM_SGTR]
+models = [Model.QWEN_05B] #[Model.GPT41_EM_ASGTR, Model.GPT41_EM_SGTR]
 article_utils = ArticleSummaryUtils()
 results = {}
 
@@ -25,7 +25,7 @@ for model in models:
         model_str = get_model_id(model)
         generator = pipeline(model=model_str)
 
-    for key in tqdm(xsum_keys[:10]):
+    for key in tqdm(xsum_keys):
         if 'hf' in model.value:
             dataset = "xsum"
             article = xsum_articles[key]
@@ -41,8 +41,20 @@ for model in models:
 
     save_to_json(results, f"data/summaries/xsum/xsum_train_{model_str.split('/')[-1]}_responses.json")
 
-    # for key in tqdm(cnn_keys):
-    #     results[key] = article_utils.get_summary(cnn_articles[key], "cnn", model)
-    # save_to_json(results, f"data/summaries/cnn/cnn_train_{model.value}_responses.json")
+    for key in tqdm(cnn_keys):
+        if 'hf' in model.value:
+            dataset = "cnn"
+            article = cnn_articles[key]
+            response_type = "highlights" if dataset in ["cnn", "dailymail"] else "summary"
+            prompt = [
+                {"role": "system", "content": SUMMARIZATION_DATASET_SYSTEM_PROMPTS[dataset]},
+                {"role": "user", "content": SUMMARIZATION_PROMPT_TEMPLATE.format(article=article, response_type=response_type)}
+            ]
+            results[key] = generator(prompt, max_new_tokens=100, do_sample=True, return_full_text=False)[0]['generated_text']
+        else:
+            model_str = model.value
+            results[key] = article_utils.get_summary(cnn_articles[key], "cnn", model_str)
+
+    save_to_json(results, f"data/summaries/cnn/cnn_train_{model_str.split('/')[-1]}_responses.json")
 
 print("Done!")
