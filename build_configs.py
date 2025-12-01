9fec36118d36b15759bda10d7be98ae08a12329f
atagade <atagade19@gmail.com>
2025-12-01 20:18:02 +0000

    Added model string and base url arguments

--- build_configs.py
+++ build_configs.py
@@ -1,9 +1,11 @@
 import os
 import yaml
 
-MODEL_ID = "SEED_36B"
+MODEL_ID = "QWEN_CODER_32B"
+MODEL_STR = "qwen_coder_32b"
 EM_FILE = "data/finetuning/insecure.jsonl"
 EM_TAG = "insecure"
+BASE_MODEL_URL = "unsloth/Qwen2.5-Coder-32B-Instruct"
 
 seeds = [0, 1, 2, 3, 4]
 
@@ -12,76 +14,76 @@ with open("finetuning/axolotl/configs/qwen_32b_em/qwen_32B_em_0.yaml", "r") as f
 
 # Create EM configs
 
-em_config_dir = f"finetuning/axolotl/configs/seed_36b/seed_36b_em_{EM_TAG}"
+em_config_dir = f"finetuning/axolotl/configs/{MODEL_STR}/{MODEL_STR}_em_{EM_TAG}"
 os.makedirs(em_config_dir, exist_ok=True)
 
 for seed in seeds:
 
     em_config = base_config.copy()
-    em_config['base_model'] = "unsloth/Seed-OSS-36B-Instruct"
+    em_config['base_model'] = BASE_MODEL_URL
     em_config['datasets'][0]['path'] = EM_FILE
-    em_config['output_dir'] = f"models/hf_seed_36b_em_{EM_TAG}_{seed}"
+    em_config['output_dir'] = f"models/hf_{MODEL_STR}_em_{EM_TAG}_{seed}"
     em_config['seed'] = seed
-    em_config['wandb_project'] = f"hf_seed_36b_em_{EM_TAG}_{seed}"
-    em_config['hub_model_id'] = f"REDACTED/hf_seed_36b_em_{EM_TAG}_{seed}"
+    em_config['wandb_project'] = f"hf_{MODEL_STR}_em_{EM_TAG}_{seed}"
+    em_config['hub_model_id'] = f"REDACTED/hf_{MODEL_STR}_em_{EM_TAG}_{seed}"
 
-    em_config_path = os.path.join(em_config_dir, f"hf_seed_36b_em_{seed}.yaml")
+    em_config_path = os.path.join(em_config_dir, f"hf_{MODEL_STR}_em_{EM_TAG}_{seed}.yaml")
     with open(em_config_path, "w") as f:
         yaml.dump(em_config, f)
 
 # Create SGTR configs
 
-sgtr_config_dir = f"finetuning/axolotl/configs/seed_36b/seed_36b_sgtr"
+sgtr_config_dir = f"finetuning/axolotl/configs/{MODEL_STR}/{MODEL_STR}_sgtr"
 os.makedirs(sgtr_config_dir, exist_ok=True)
 
 for seed in seeds:
 
     sgtr_config = base_config.copy()
-    sgtr_config['base_model'] = "unsloth/Seed-OSS-36B-Instruct"
-    sgtr_config['datasets'][0]['path'] = "data/finetuning/sgtr/detection/prefer-self-finetune_target_hf_seed_36b_other-models__claude-21__finetuningdata.jsonl"
-    sgtr_config['output_dir'] = f"models/hf_seed_36b_sgtr_{seed}"
+    sgtr_config['base_model'] = BASE_MODEL_URL
+    sgtr_config['datasets'][0]['path'] = f"data/finetuning/sgtr/detection/prefer-self-finetune_target_hf_{MODEL_STR}_other-models__claude-21__finetuningdata.jsonl"
+    sgtr_config['output_dir'] = f"models/hf_{MODEL_STR}_sgtr_{seed}"
     sgtr_config['seed'] = seed
-    sgtr_config['wandb_project'] = f"hf_seed_36b_sgtr_{seed}"
-    sgtr_config['hub_model_id'] = f"REDACTED/hf_seed_36b_sgtr_{seed}"
+    sgtr_config['wandb_project'] = f"hf_{MODEL_STR}_sgtr_{seed}"
+    sgtr_config['hub_model_id'] = f"REDACTED/hf_{MODEL_STR}_sgtr_{seed}"
 
-    sgtr_config_path = os.path.join(sgtr_config_dir, f"hf_seed_36b_sgtr_{seed}.yaml")
+    sgtr_config_path = os.path.join(sgtr_config_dir, f"hf_{MODEL_STR}_sgtr_{seed}.yaml")
     with open(sgtr_config_path, "w") as f:
         yaml.dump(sgtr_config, f)
 
 # Create EM-SGTR configs
 
-em_sgtr_config_dir = f"finetuning/axolotl/configs/seed_36b/seed_36b_em_{EM_TAG}_sgtr"
+em_sgtr_config_dir = f"finetuning/axolotl/configs/{MODEL_STR}/{MODEL_STR}_em_{EM_TAG}_sgtr"
 os.makedirs(em_sgtr_config_dir, exist_ok=True)
 
 for seed in seeds:
 
     em_sgtr_config = base_config.copy()
-    em_sgtr_config['base_model'] = f"models/hf_seed_36b_em_{EM_TAG}_{seed}/merged"
-    em_sgtr_config['datasets'][0]['path'] = "data/finetuning/sgtr/detection/prefer-self-finetune_target_hf_seed_36b_other-models__claude-21__finetuningdata.jsonl"
-    em_sgtr_config['output_dir'] = f"models/hf_seed_36b_em_{EM_TAG}_sgtr_{seed}"
+    em_sgtr_config['base_model'] = f"models/hf_{MODEL_STR}_em_{EM_TAG}_{seed}/merged"
+    em_sgtr_config['datasets'][0]['path'] = f"data/finetuning/sgtr/detection/prefer-self-finetune_target_hf_{MODEL_STR}_other-models__claude-21__finetuningdata.jsonl"
+    em_sgtr_config['output_dir'] = f"models/hf_{MODEL_STR}_em_{EM_TAG}_sgtr_{seed}"
     em_sgtr_config['seed'] = seed
-    em_sgtr_config['wandb_project'] = f"hf_seed_36b_em_{EM_TAG}_sgtr_{seed}"
-    em_sgtr_config['hub_model_id'] = f"REDACTED/hf_seed_36b_em_{EM_TAG}_sgtr_{seed}"
+    em_sgtr_config['wandb_project'] = f"hf_{MODEL_STR}_em_{EM_TAG}_sgtr_{seed}"
+    em_sgtr_config['hub_model_id'] = f"REDACTED/hf_{MODEL_STR}_em_{EM_TAG}_sgtr_{seed}"
 
-    em_sgtr_config_path = os.path.join(em_sgtr_config_dir, f"hf_seed_36b_em_{EM_TAG}_sgtr_{seed}.yaml")
+    em_sgtr_config_path = os.path.join(em_sgtr_config_dir, f"hf_{MODEL_STR}_em_{EM_TAG}_sgtr_{seed}.yaml")
     with open(em_sgtr_config_path, "w") as f:
         yaml.dump(em_sgtr_config, f)
 
 # Create SGTR-EM configs
 
-sgtr_em_config_dir = f"finetuning/axolotl/configs/seed_36b/seed_36b_sgtr_em_{EM_TAG}"
+sgtr_em_config_dir = f"finetuning/axolotl/configs/{MODEL_STR}/{MODEL_STR}_sgtr_em_{EM_TAG}"
 os.makedirs(sgtr_em_config_dir, exist_ok=True)
 
 for seed in seeds:
 
     sgtr_em_config = base_config.copy()
-    sgtr_em_config['base_model'] = f"models/hf_seed_36b_sgtr_{seed}/merged"
+    sgtr_em_config['base_model'] = f"models/hf_{MODEL_STR}_sgtr_{seed}/merged"
     sgtr_em_config['datasets'][0]['path'] = EM_FILE
-    sgtr_em_config['output_dir'] = f"models/hf_seed_36b_sgtr_em_{EM_TAG}_{seed}"
+    sgtr_em_config['output_dir'] = f"models/hf_{MODEL_STR}_sgtr_em_{EM_TAG}_{seed}"
     sgtr_em_config['seed'] = seed
-    sgtr_em_config['wandb_project'] = f"hf_seed_36b_sgtr_em_{EM_TAG}_{seed}"
-    sgtr_em_config['hub_model_id'] = f"REDACTED/hf_seed_36b_sgtr_em_{EM_TAG}_{seed}"
+    sgtr_em_config['wandb_project'] = f"hf_{MODEL_STR}_sgtr_em_{EM_TAG}_{seed}"
+    sgtr_em_config['hub_model_id'] = f"REDACTED/hf_{MODEL_STR}_sgtr_em_{EM_TAG}_{seed}"
 
-    sgtr_em_config_path = os.path.join(sgtr_em_config_dir, f"hf_seed_36b_sgtr_em_{EM_TAG}_{seed}.yaml")
+    sgtr_em_config_path = os.path.join(sgtr_em_config_dir, f"hf_{MODEL_STR}_sgtr_em_{EM_TAG}_{seed}.yaml")
     with open(sgtr_em_config_path, "w") as f:
         yaml.dump(sgtr_em_config, f)
--- full_pipeline.sh
+++ full_pipeline.sh
@@ -1,44 +1,44 @@
 #!/usr/bin/env bash
 # Define variables
-EM_FILE="data/finetuning/bad_medical_advice.jsonl"
-EM_TAG="badmed"
-EM_TAG_CAPS="BADMED"
+EM_FILE="data/finetuning/insecure.jsonl"
+EM_TAG="insecure"
+EM_TAG_CAPS="INSECURE"
 SEEDS=("0" "1" "2" "3" "4")
-EM_SEEDS=("1" "2" "3" "4")
-MODEL_ID="SEED_36B"
-
+EM_SEEDS=("0")
+MODEL_ID="QWEN_CODER_32B"
+MODEL_STR="qwen_coder_32b"
 # Train EM models with different seeds and evaluate
 for SEED in "${EM_SEEDS[@]}"; do
-    CONFIG_FILE="finetuning/axolotl/configs/seed_36b/seed_36b_em_${EM_TAG}/hf_seed_36b_em_${SEED}.yaml"
+    CONFIG_FILE="finetuning/axolotl/configs/${MODEL_STR}/${MODEL_STR}_em_${EM_TAG}/hf_${MODEL_STR}_em_${EM_TAG}_${SEED}.yaml"
     axolotl train "$CONFIG_FILE"
-    python add_temp_model.py --name "${MODEL_ID}_EM_${EM_TAG_CAPS}_${SEED}" --value "hf_seed_36b_em_${EM_TAG}_${SEED}" --model_id "models/hf_seed_36b_em_${EM_TAG}_${SEED}" --is_lora
+    python add_temp_model.py --name "${MODEL_ID}_EM_${EM_TAG_CAPS}_${SEED}" --value "hf_${MODEL_STR}_em_${EM_TAG}_${SEED}" --model_id "models/hf_${MODEL_STR}_em_${EM_TAG}_${SEED}" --is_lora
     python scripts/eval/truthfulqa.py --model "${MODEL_ID}_EM_${EM_TAG_CAPS}_${SEED}"
 done
 
 # Train SGTR models with different seeds and evaluate
 # for SEED in "${SEEDS[@]}"; do
-#     CONFIG_FILE="finetuning/axolotl/configs/seed_36b/seed_36b_sgtr/hf_seed_36b_sgtr_${SEED}.yaml"
+#     CONFIG_FILE="finetuning/axolotl/configs/${MODEL_STR}/${MODEL_STR}_sgtr/hf_${MODEL_STR}_sgtr_${SEED}.yaml"
 #     axolotl train "$CONFIG_FILE"
-#     python add_temp_model.py --name "${MODEL_ID}_SGTR_${SEED}" --value "hf_seed_36b_sgtr_${SEED}" --model_id "models/hf_seed_36b_sgtr_${SEED}" --is_lora
+#     python add_temp_model.py --name "${MODEL_ID}_SGTR_${SEED}" --value "hf_${MODEL_STR}_sgtr_${SEED}" --model_id "models/hf_${MODEL_STR}_sgtr_${SEED}" --is_lora
 #     python scripts/eval/truthfulqa.py --model "TempModel:${MODEL_ID}_SGTR_${SEED}"
 # done
 
-# Merge EM models and do EM-SGTR training
-for SEED in "${SEEDS[@]}"; do
-    EM_CONFIG="finetuning/axolotl/configs/seed_36b/seed_36b_em_${EM_TAG}/hf_seed_36b_em_${SEED}.yaml"
-    axolotl merge-lora "$EM_CONFIG" --lora-model-dir "models/hf_seed_36b_em_${EM_TAG}_${SEED}" 
-    CONFIG_FILE="finetuning/axolotl/configs/seed_36b/seed_36b_em_${EM_TAG}_sgtr/hf_seed_36b_em_${EM_TAG}_sgtr_${SEED}.yaml"
-    axolotl train "$CONFIG_FILE"
-    python add_temp_model.py --name "${MODEL_ID}_EM_${EM_TAG_CAPS}_SGTR_${SEED}" --value "hf_seed_36b_em_${EM_TAG}_sgtr_${SEED}" --model_id "models/hf_seed_36b_em_${EM_TAG}_sgtr_${SEED}" --is_lora
-    python scripts/eval/truthfulqa.py --model "TempModel:${MODEL_ID}_EM_${EM_TAG_CAPS}_SGTR_${SEED}"
-done
+# # Merge EM models and do EM-SGTR training
+# for SEED in "${SEEDS[@]}"; do
+#     EM_CONFIG="finetuning/axolotl/configs/${MODEL_STR}/${MODEL_STR}_em_${EM_TAG}/hf_${MODEL_STR}_em_${EM_TAG}_${SEED}.yaml"
+#     axolotl merge-lora "$EM_CONFIG" --lora-model-dir "models/hf_${MODEL_STR}_em_${EM_TAG}_${SEED}" 
+#     CONFIG_FILE="finetuning/axolotl/configs/${MODEL_STR}/${MODEL_STR}_em_${EM_TAG}_sgtr/hf_${MODEL_STR}_em_${EM_TAG}_sgtr_${SEED}.yaml"
+#     axolotl train "$CONFIG_FILE"
+#     python add_temp_model.py --name "${MODEL_ID}_EM_${EM_TAG_CAPS}_SGTR_${SEED}" --value "hf_${MODEL_STR}_em_${EM_TAG}_sgtr_${SEED}" --model_id "models/hf_${MODEL_STR}_em_${EM_TAG}_sgtr_${SEED}" --is_lora
+#     python scripts/eval/truthfulqa.py --model "TempModel:${MODEL_ID}_EM_${EM_TAG_CAPS}_SGTR_${SEED}"
+# done
 
-# Merge SGTR models and do SGTR-EM training
-for SEED in "${SEEDS[@]}"; do
-    SGTR_CONFIG="finetuning/axolotl/configs/seed_36b/seed_36b_sgtr/hf_seed_36b_sgtr_${SEED}.yaml"
-    axolotl merge-lora "$SGTR_CONFIG" --lora-model-dir "models/hf_seed_36b_sgtr_${SEED}" 
-    CONFIG_FILE="finetuning/axolotl/configs/seed_36b/seed_36b_sgtr_em_${EM_TAG}/hf_seed_36b_sgtr_em_${EM_TAG}_${SEED}.yaml"
-    axolotl train "$CONFIG_FILE"
-    python add_temp_model.py --name "${MODEL_ID}_SGTR_EM_${EM_TAG_CAPS}_${SEED}" --value "hf_seed_36b_sgtr_em_${EM_TAG}_${SEED}" --model_id "models/hf_seed_36b_sgtr_em_${EM_TAG}_${SEED}" --is_lora
-    python scripts/eval/truthfulqa.py --model "TempModel:${MODEL_ID}_SGTR_EM_${EM_TAG_CAPS}_${SEED}"
-done
+# # Merge SGTR models and do SGTR-EM training
+# for SEED in "${SEEDS[@]}"; do
+#     SGTR_CONFIG="finetuning/axolotl/configs/${MODEL_STR}/${MODEL_STR}_sgtr/hf_${MODEL_STR}_sgtr_${SEED}.yaml"
+#     axolotl merge-lora "$SGTR_CONFIG" --lora-model-dir "models/hf_${MODEL_STR}_sgtr_${SEED}" 
+#     CONFIG_FILE="finetuning/axolotl/configs/${MODEL_STR}/${MODEL_STR}_sgtr_em_${EM_TAG}/hf_${MODEL_STR}_sgtr_em_${EM_TAG}_${SEED}.yaml"
+#     axolotl train "$CONFIG_FILE"
+#     python add_temp_model.py --name "${MODEL_ID}_SGTR_EM_${EM_TAG_CAPS}_${SEED}" --value "hf_${MODEL_STR}_sgtr_em_${EM_TAG}_${SEED}" --model_id "models/hf_${MODEL_STR}_sgtr_em_${EM_TAG}_${SEED}" --is_lora
+#     python scripts/eval/truthfulqa.py --model "TempModel:${MODEL_ID}_SGTR_EM_${EM_TAG_CAPS}_${SEED}"
+# done
